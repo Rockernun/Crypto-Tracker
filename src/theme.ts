7c06dfda1cf095ccc9bbb052baae0eecf89534c4
Eokji coding <byunguk98@naver.com>
2024-01-19 21:36:44 +0900

    Theme 설정

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,14 @@
 import { DefaultTheme } from "styled-components";
 
-export const theme: DefaultTheme = {
+export const darkTheme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  accentColor: "white",
+  textColor: "white",
+  accentColor: "#00E396",
+  cardBgColor: "transparent",
+};
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f1f2f6",
+  textColor: "black",
+  accentColor: "#70a1ff",
+  cardBgColor: "transparent",
 };
