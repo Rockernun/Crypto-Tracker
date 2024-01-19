9651b47a413f4f29a988e4269917d4da9e264ff7
Eokji coding <byunguk98@naver.com>
2024-01-19 12:47:44 +0900

    Loader Component, Object 전달
    Loader Styled Component를 생성하고, Link를 통해 Object, 데이터 자체 그대로 Coin 페이지에 보냈다.

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -22,10 +22,12 @@ const Coin = styled.li`
   color: ${(props) => props.theme.bgColor};
   border-radius: 15px;
   margin-bottom: 10px;
+  padding: 5px;
   a {
+    display: flex;
+    align-items: center;
     padding: 15px;
     transition: color 0.3s ease-in-out;
-    display: block;
   }
   &:hover{
     a {
@@ -37,6 +39,17 @@ const Coin = styled.li`
 const Title = styled.h1`
   font-size: 48px;
   color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Img = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 8px;
 `;
 
 interface CoinInterface {
@@ -66,12 +79,19 @@ function Coins() {
       <Header>
         <Title>Coin</Title>
       </Header>
-      {loading ? "Loading..." : 
-      <CoinsList>
-        {coins.map(coin => 
+      {loading ? <Loader>Loading...</Loader> : 
+    <CoinsList>
+      {coins.map(coin => 
           (<Coin key={coin.id}>
-            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-            </Coin>))}
+            <Link to={{
+              pathname: `/${coin.id}`,
+              state: { name: coin.name },
+            }}
+            >
+            <Img src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`} />
+              {coin.name} &rarr;
+            </Link>
+          </Coin>))}
       </CoinsList>}
     </Container>
   )
